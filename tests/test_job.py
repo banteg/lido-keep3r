f5bdd4d40dbb695ea252c997a242ce5efd60efaf
banteg <banteeg@gmail.com>
2020-12-24 21:00:03 +0300

    fix: test output

--- tests/test_job.py
+++ tests/test_job.py
@@ -2,10 +2,11 @@ from brownie import Wei
 
 
 def test_job(kpr, lido, job, kpr_whale, user, keeper):
-    target = Wei("2048 ether")
+    target = Wei("2500 ether")
     buffered = lido.getBufferedEther()
     if buffered < target:
         lido.submit(lido, {"from": user, "amount": target - buffered})
+    buffered = lido.getBufferedEther()
     kpr.addJob(job)
     kpr.approve(kpr, "100 ether", {"from": kpr_whale})
     kpr.addCredit(kpr, job, "100 ether", {"from": kpr_whale})
