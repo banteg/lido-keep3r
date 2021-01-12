492f36819c7f017ea03ae450670c48525c8e3b92
banteg <banteeg@gmail.com>
2021-01-12 17:52:19 +0300

    feat: add lp credit script

--- scripts/keeper.py
+++ scripts/keeper.py
@@ -1,5 +1,8 @@
 import click
-from brownie import LidoJob, Contract, Wei, accounts, chain
+from brownie import LidoJob, Contract, Wei, accounts, chain, network
+from brownie.network.gas.strategies import GasNowScalingStrategy
+
+network.gas_price(GasNowScalingStrategy())
 
 
 def get_account():
@@ -35,3 +38,39 @@ def add_credit():
         keep3r.approve(keep3r, 2 ** 256 - 1)
     keep3r.addCredit(keep3r, job, amount)
     print("job credits:", keep3r.credits(job, keep3r).to("ether"))
+
+
+def add_lp_credit():
+    """
+    1. run to add liquidity to job
+    2. wait 3 days
+    3. run again to apply credit to job
+    """
+    user = get_account()
+    job = get_job(user)
+    keep3r = Contract("0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44", owner=user)
+    lp = Contract("0x79e0d4858af8071349469b6589a3c23c1fe1586e", owner=user)
+
+    click.secho(f"job credits: {keep3r.credits(job, keep3r).to('ether')}", fg="green")
+    assert keep3r.liquidityAccepted(lp), "liquidity not accepted"
+
+    bonded = keep3r.liquidityApplied(user, lp, job)
+    now = chain[-1].timestamp
+    if bonded > 0:
+        when = f"in {bonded - now}s" if now < bonded else "now"
+        click.secho(f"found bonded liquidity, it can be finalized {when}", fg="yellow")
+        if bonded < now:
+            keep3r.applyCreditToJob(user, lp, job)
+            click.secho(
+                f"job credits: {keep3r.credits(job, keep3r).to('ether')}", fg="green"
+            )
+        return
+
+    print("sushi ldo/kp3r lp balance:", lp.balanceOf(user).to("ether"))
+    amount = click.prompt("top up amount", default=lp.balanceOf(user).to("ether"))
+    amount = Wei(f"{amount} ether")
+
+    if lp.allowance(user, keep3r) < amount:
+        lp.approve(keep3r, 2 ** 256 - 1)
+
+    keep3r.addLiquidityToJob(lp, job, amount)
